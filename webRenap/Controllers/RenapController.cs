9673ad87cd2ac9347ff346b0869d68a7c972646d
luis <lfflorian4@gmail.com>
2018-05-31 23:42:06 -0600

    inicialización de proyecto y vistas añadidas

---
+++ webRenap/Controllers/RenapController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webRenap.Controllers
+{
+    public class RenapController : Controller
+    {
+        // GET: Renap
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Solicitud()
+        {
+            return View();
+        }
+
+        public ActionResult Certificado()
+        {
+            return View();
+        }
+    }
+}
